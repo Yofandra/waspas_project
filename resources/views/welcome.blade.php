a23833ce6317bf4f64a8fbc574d6857a48358f4f
Amaliastf <125347989+Amaliastf@users.noreply.github.com>
2023-11-30 23:34:56 +0700

    home1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
 
         <!-- Styles -->
         <style>
@@ -16,13 +19,50 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="flex flex-nowrap">
-            <div><h1 class="text-3xl font-bold ">
-                Hello world!
-              </h1></div>
-            <div>02</div>
-            <div>03</div>
-          </div>
-        
+        <nav style="background-color: #726274;" >
+        <div class="flex flex-wrap">
+            <div class="items-start justify-start ml-12 p-2 text-purple-50 font-sans">
+                Call : +62-812-3198-0448
+            </div>
+            <div class="items-end justify-end mr-12 mx-auto p-2 text-purple-50 font-sans">
+                Email: waspasmethod@gmail.com
+            </div>  
+        </div>       
+        </nav>
+        <nav class="bg-gray-50 border-gray-50 dark:bg-gray-50">
+
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse ml-6">
+                <img src="{{ asset('docs/logoWaspas.png') }}" class="h-8" alt="Flowbite Logo" />
+
+                <!-- <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span> -->
+            </a>
+            <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-50 dark:bg-gray-50 md:dark:bg-gray-50 dark:border-gray-700">
+                <li>
+                <a href="#" class="block py-2 px-3 text-purple-500 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-purple-500 md:dark:text-blue-500" aria-current="page">Home</a>
+                </li>
+                <li>
+                <a href="#" class="block py-2 px-3 text-purple-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-purple-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-purple-500 md:dark:hover:bg-transparent">About</a>
+                </li>
+                <li>
+                <a href="#" class="block py-2 px-3 text-purple-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-purple-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-purple-500 md:dark:hover:bg-transparent">Setting</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+
+        <div class="w-screen h-screen">
+                <img src="{{ asset('docs/bg.jpg') }}" class="h-8 w-screen h-screen" alt="Flowbite Logo"  />
+                <a></a>
+        </div>
+ 
     </body>
 </html>
